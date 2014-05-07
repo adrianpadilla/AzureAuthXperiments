11ac193b1ee7a50b0958cc5a2faf756eb1e153ce
Adrian Padilla Vega <adrian@adrianpadilla.net>
2014-05-07 09:30:43 -0700

    Created intial site

---
+++ AzureAuthXperiments/ExampleWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace ExampleWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
